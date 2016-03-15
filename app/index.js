ebd9600a58a3e9d32d5d9813163009bd6728c86b
Petro Tarasenko <pereter92@gmail.com>
2016-03-15 18:26:03 +0200

    react router added for compontns routing

--- app/index.js
+++ app/index.js
@@ -1,59 +1,10 @@
-var USER_DATA = {
-    name: "Petro",
-    username: "peetwiter",
-    image: "https://avatars2.githubusercontent.com/u/3470300?v=3&s=460"
-}
-
-https://avatars2.githubusercontent.com/u/3470300?v=3&s=460
 var React = require("react");
 var ReactDOM = require("react-dom");
 
+var routes = require("./config/routes");
 
-var ProfileImage = React.createClass({
-    render: function() {
-        return(
-            <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-        )
-    }
-});
-
-var ProfileName = React.createClass({
-    render: function() {
-        return(
-            <div>
-                {this.props.name}
-            </div>
-
-        )
-    }
-});
-
-var ProfileLink = React.createClass({
-    render: function() {
-        return(
-            <div>
-                <a href={'https://github.com/'+ this.props.username}>
-                    {this.props.username }
-                </a>
-            </div>
-
-        )
-    }
-});
-
-var Avatar = React.createClass({
-    render: function() {
-        return(
-            <div>
-                <ProfileImage imageUrl={this.props.user.image} />
-                <ProfileName name={this.props.user.name} />
-                <ProfileLink username={this.props.user.username} />
-            </div>
-        )
-    }
-});
 
 ReactDOM.render(
-    <Avatar user={USER_DATA}/>,
+    routes,
     document.getElementById("app")
 )
