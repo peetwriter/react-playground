8a22e805dc45545b13dccb598e001661da74460b
Petr Tarasenko <pereter92@gmail.com>
2016-03-21 18:40:57 +0200

    confirmBattle container added

--- app/config/routes.js
+++ app/config/routes.js
@@ -9,6 +9,7 @@ var hashHistory = ReactRouter.HashHistory;
 var Main = require("../components/main");
 var Home = require("../components/home");
 var PromtContainer = require("../containers/promptContainer");
+var ConfirmBattleContainer = require("../containers/confirmBattleContainer");
 
 var routes = (
     <Router history={hashHistory}>
@@ -16,6 +17,7 @@ var routes = (
             <IndexRoute path="" component={Home}/>
             <Route path="playerOne" header="Player One" component={PromtContainer}></Route>
             <Route path="playerTwo/:playerOne" header="Player Two" component={PromtContainer}></Route>
+            <Route path="battle" component={ConfirmBattleContainer}></Route>
         </Route>
     </Router>
 )
---
+++ app/containers/confirmBattleContainer.js
@@ -0,0 +1,27 @@
+var React = require('react');
+var ConfirmBattle = require('../components/confirmBattle');
+
+
+var ConfirmBattleContainer = React.createClass({
+    contextTypes: {
+        router: React.PropTypes.object.isRequired
+    },
+    getInitialState: function () {
+        return {
+            isLoading: true,
+            playersInfo: []
+        }
+    },
+    componentDidMount: function () {
+        var query = this.props.location.query;
+    },
+    render: function (){
+        return (
+            <ConfirmBattle
+                isLoading={this.state.isLoading}
+                playersInfo={this.state.playersInfo}/>
+        );
+    }
+});
+
+module.exports = ConfirmBattleContainer;
