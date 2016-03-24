220f43f7d828f0a6f691a9f8e28b62742f3d71cb
Petr Tarasenko <pereter92@gmail.com>
2016-03-24 22:44:22 +0200

    Users Detail Wrapper added

--- app/components/confirmBattle.js
+++ app/components/confirmBattle.js
@@ -3,6 +3,7 @@ var styles = require("../styles");
 var Link = require("react-router").Link;
 
 var UserDetails = require("./userDetails")
+var UserDetailsWrapper = require("./userDetailsWrapper")
 
 var puke = function (object){
     return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -15,14 +16,12 @@ function ConfirmBattle (props) {
     : <div className="jumbotrone col-sm-12 text-center" style={styles.transparentBg}>
         <h1>Confirm Players</h1>
         <div className="col-sm-8 col-sm-offset-2">
-            <div className="col-sm-6">
-                <p className="lead">Player 1</p>
+            <UserDetailsWrapper header="Player One">
                 <UserDetails info={props.playersInfo[0]}/>
-            </div>
-            <div className="col-sm-6">
-                <p className="lead">Player 2</p>
+            </UserDetailsWrapper>
+            <UserDetailsWrapper header="Player Two">
                 <UserDetails info={props.playersInfo[1]}/>
-            </div>
+            </UserDetailsWrapper>
         </div>
         <div className="col-sm-8 col-sm-offset-2">
             <div className="col-sm-12" style={styles.space}>
