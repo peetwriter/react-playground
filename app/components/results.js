efaae04678a4c85eba02974042c89fe2d8d61c90
Petr Tarasenko <pereter92@gmail.com>
2016-03-27 21:44:32 +0300

    results container rendering results

--- app/components/results.js
+++ app/components/results.js
@@ -1,13 +1,27 @@
 var React = require("react");
 var PropTypes = React.PropTypes;
+var transparentBg =  require('../styles').transparentBg;
 
-function puke (obj) {
-    return <pre>{JSON.stringify(obj, 2, ' ')}</pre>
-}
+var UserDetails = require("./userDetails")
+var UserDetailsWrapper = require("./userDetailsWrapper")
 
 function Results (props) {
+    var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+    var losingIndex = winningIndex === 1 ? 0 : 1;
     return (
-        <div> Results: {puke(props)}</div>
+        <div className="jumbotrone col-sm-12 text-center" style={transparentBg}>
+            <h1>
+                Results
+            </h1>
+            <div className="col-sm-8 col-sm-offset-2">
+                <UserDetailsWrapper header="Winner">
+                    <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]}></UserDetails>
+                </UserDetailsWrapper>
+                <UserDetailsWrapper header="Looser">
+                    <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]}></UserDetails>
+                </UserDetailsWrapper>
+            </div>
+        </div>
     )
 }
 
